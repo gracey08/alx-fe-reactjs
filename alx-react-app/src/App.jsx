08b3786c8887a177e2edf947cd1c6cade9c07bde
gracey08 <mayungbe95@gmail.com>
2025-02-14 13:22:50 +0100

    new update app

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,13 +3,20 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
       <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
-  );
+  );   
+  
 }
 //   const [count, setCount] = useState(0)
 
