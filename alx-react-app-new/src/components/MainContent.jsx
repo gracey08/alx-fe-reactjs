7e3ba51f7d68b466fd755295bf8fe83ef7df4dec
gracey08 <mayungbe95@gmail.com>
2025-02-22 02:20:39 +0100

    new update app

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MainContent = () => {
+  return (
+<main  style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
+  <p>I love to visit New York, Paris, and Tokyo.</p>
+</main>
+
+);
+};
+
+export default MainContent;
