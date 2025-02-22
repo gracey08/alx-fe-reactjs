ec64ec042a9207a88e6b0a671e15105dfdbea583
gracey08 <mayungbe95@gmail.com>
2025-02-22 23:44:26 +0100

    new update app

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -8,7 +8,7 @@ import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
 import UserContext from './UserContext';
-console.log('UserContext:', UserContext); // Should log the context object
+console.log('UserContext:', UserContext.js); // Should log the context object
 import ProfilePage from './ProfilePage.jsx';
 
 function App() {
--- alx-react-app-props/src/components/UserDetails.jsx
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -1,6 +1,6 @@
 // src/UserDetails.jsx
 import { useContext } from 'react'; // Import useContext
-import UserContext from './UserContext.js'; // Import UserContext
+import UserContext from '../UserContext.js'; // Import UserContext
 
 function UserDetails() {
   const userData = useContext(UserContext); // Consume context instead of props
