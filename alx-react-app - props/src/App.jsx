85fb1e5399614ecae689d90f420fa536f8917de6
gracey08 <mayungbe95@gmail.com>
2025-02-22 02:50:21 +0100

    new update app

--- alx-react-app - props/src/App.jsx
+++ alx-react-app - props/src/App.jsx
@@ -1,12 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import WelcomeMessage from './components/WelcomeMessage'
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-import UserProfile from './components/UserProfile';
 import ProfilePage from './ProfilePage';
 
 function App() {
