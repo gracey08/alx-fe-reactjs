47c2624a370328a30f63d156555a088e2aba9f44
gracey08 <mayungbe95@gmail.com>
2025-02-22 16:01:32 +0100

    new update app

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -7,7 +7,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
-import UserContext from './UserContext';
+import UserContext from './UserContext.js';
 import ProfilePage from './ProfilePage.jsx';  // Fixed import with extension
 
 function App() {
