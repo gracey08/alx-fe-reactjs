cf72b6b601c3bb19b424633990a03ecda931fd3c
gracey08 <mayungbe95@gmail.com>
2025-02-28 07:17:16 +0100

    new update app

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -1,5 +1,5 @@
 // src/components/RecipeDetails.jsx
-import useRecipeStore from '../stores/recipeStore';
+import useRecipeStore from './recipeStore';
 import EditRecipeForm from './EditRecipeForm';
 import DeleteRecipeButton from './DeleteRecipeButton';
 
