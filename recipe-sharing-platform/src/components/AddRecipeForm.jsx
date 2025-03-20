9e0e9fe0424305eaf193bc450ed7bee3bcb69869
gracey08 <mayungbe95@gmail.com>
2025-03-20 21:35:02 +0100

    new tailwindcss project

--- recipe-sharing-platform/src/components/AddRecipeForm.jsx
+++ recipe-sharing-platform/src/components/AddRecipeForm.jsx
@@ -10,7 +10,8 @@ const AddRecipeForm = () => {
   const [errors, setErrors] = useState({});
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
+    const name = e.target.name;
+    const value = e.target.value;
     setFormData((prev) => ({
       ...prev,
       [name]: value,
